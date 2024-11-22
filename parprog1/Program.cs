3916308eb828cb78eafc88be432b36f3e1129d61
YanissCedriik <167817702+YanissCedriik@users.noreply.github.com>
2024-11-22 14:24:02 +0100

    Initial Commit

---
+++ parprog1/Program.cs
@@ -0,0 +1,69 @@
+﻿// Lage objekter (Yaniss, Nickolas)
+// Objektene skal ha verdier som Navn, alder, og egenskaper.
+// Objektene skal også ha ting de liker å gjøre (string som forteller om en ting)
+
+//Meny hvor du velge person og få informasjon om dem.
+
+
+using System;
+using parprog1;
+
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+     
+        List<Person> people = new List<Person>();
+
+       
+        people.Add(new Person("Yaniss", 22, "blonde", "golf"));
+        people.Add(new Person("Nickolas", 21, "black", "reading"));
+
+
+        Console.WriteLine("People List");
+        Console.WriteLine("-----------");
+
+        while (true)
+        {
+
+
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                Console.WriteLine($"{i}. {people[i].Name}");
+            }
+
+            Console.WriteLine("Select index of person for more (0-1)");
+
+            string userInput = Console.ReadLine();
+
+            if (userInput == "0")
+            {
+                int index = Convert.ToInt32(userInput);
+                Console.WriteLine(
+                    $"Person : {people[index].Name} is {people[index].Age} years old. and likes to {people[index].Hobbies}.");
+                Console.WriteLine($"Persons hairColor is {people[index].HairColor}!");
+            }
+            else if (userInput == "1")
+            {
+                int index = Convert.ToInt32(userInput);
+                Console.WriteLine(
+                    $"Person : {people[index].Name} is {people[index].Age} years old. and likes to {people[index].Hobbies}.");
+                Console.WriteLine($"Persons hairColor is {people[index].HairColor}!");
+            }
+            else if (userInput == "exit")
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+
+
+}
+
+
+
+
+
---
+++ parprog1/person.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace parprog1
+{
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string HairColor { get; set; }
+        public string Hobbies { get; set; }
+
+        public Person(string name, int age, string hairColor, string hobbies)
+        {
+            Name = name;
+            Age = age;
+            HairColor = hairColor;
+            Hobbies = hobbies;
+        }
+
+    }
+
+}
